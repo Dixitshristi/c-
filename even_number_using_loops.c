0767004cb7d917220bb74cfe5134aa3c42889da1
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-26 00:20:46 +0530

    even_number_using_loops.c

---
+++ even_number_using_loops.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    for (int i=0; i <=100; i=i+2)
+    {
+        if (i%2==0)
+        {
+            printf("%d ",i);
+        }
+        
+    }
+    return 0;
+}

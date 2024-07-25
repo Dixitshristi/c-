a1b5e0bf62d958cbca61dfeb6a519cfe758f655a
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-26 00:09:59 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int a;
+    printf("enter a value\n");
+    scanf("%d\n",&a);
+
+    int b;
+    printf("enter b value\n");
+    scanf("%d\n",&b);
+
+    int sum;
+    printf("the sum of a and b is %d",a+b);
+
+    return 0;
+}
+
+
